47810b0f103ae2d4bfeaf30f61a5f60a7e6d9705
Benoit Guillod <bguillod@gmail.com>
2019-05-10 12:11:55 +0200

    changing figure path

--- init.R
+++ init.R
@@ -7,5 +7,5 @@ library(tidyverse)
 library(foreach)
 options(stringsAsFactors = FALSE)
 
-FIGS_PATH <- file.path(DATA_PATH_FLDCAL, "figs")
+FIGS_PATH <- file.path("/Users/bguillod/Dropbox/ETH_WORK/figs")
 if (!dir.exists(FIGS_PATH)) dir.create(FIGS_PATH)
