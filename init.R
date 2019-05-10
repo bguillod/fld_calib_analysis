3a8cbfff855b284ac9150f4a9aca67f89e010768
Benoit Guillod <bguillod@gmail.com>
2019-05-10 18:00:12 +0200

    Additional theme updates

--- init.R
+++ init.R
@@ -11,4 +11,6 @@ FIGS_PATH <- file.path("/Users/bguillod/Dropbox/ETH_WORK/figs")
 if (!dir.exists(FIGS_PATH)) dir.create(FIGS_PATH)
 
 theme_set(theme_light())
-theme_update(strip.text.x = element_text(colour = "black"),strip.background=element_rect(fill="grey85"))
+theme_update(strip.text.x = element_text(colour = "black"), strip.background=element_rect(fill="grey85"),
+             legend.position="bottom", legend.box = "horizontal",
+             plot.title = element_text(hjust = 0.5))
