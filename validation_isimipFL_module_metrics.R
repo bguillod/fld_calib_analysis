c4a286d114d393755adfa587c19c64e0bf1fbd81
bguillod <benoit.guillod@ouce.ox.ac.uk>
2019-07-10 21:20:35 +0200

    AED bias computation function

--- validation_isimipFL_module_metrics.R
+++ validation_isimipFL_module_metrics.R
@@ -32,11 +32,43 @@ rmse_of_yearly_by_country <- function(df) {
     output <-  a %>% group_by(country, damage_source) %>% select(-year) %>%
         # select(-year, -used_in_) %>%
         summarise_if(is.numeric, ~sqrt(mean(.,na.rm=T))) %>%
-        gather(3:ncol(.),key="dataset",value="damage") %>%
+        gather(3:ncol(.),key="dataset",value="rmse") %>%
         ungroup()
     return(output)
 }
     
+AEDbias_of_yearly_by_country <- function(df) {
+    # return the 
+    # group by damage function (without EM-DAT)
+    # df_grouped <- df %>% filter(damage_source!="EM-DAT") %>% group_by(damage_source)
+    if ("rt" %in% names(df)) {
+        df <- df %>% select(-rt)
+    }
+    # put each model in a column, append EM-DAT
+    df_grouped <- df %>%
+        filter(damage_source!="EM-DAT") %>%
+        # group_by(damage_source)
+        spread(dataset, damage)%>%
+        left_join(df %>%
+                      filter(damage_source=="EM-DAT") %>%
+                      spread(dataset, damage) %>%
+                      select(-damage_source,-region,-used_in_calibration),by=c("country","year"))
+    # first, keep only years and country with data in all models and damage_source to avoid sampling biases
+    
+    # second, compute squared difference between each model and EM-DAT
+    a <- df_grouped %>% select(-country, -year, -used_in_calibration,-damage_source,-region) %>%
+        map(.y=df_grouped$`EM-DAT`, .f=~(. - .y)**2) %>% as_tibble() %>%
+        select(-`EM-DAT`) %>% 
+        bind_cols(df_grouped[1:5],.)
+    # second, compute root of average per country (models are in columns)
+    output <-  a %>% group_by(country, damage_source) %>% select(-year) %>%
+        # select(-year, -used_in_) %>%
+        summarise_if(is.numeric, ~sqrt(mean(.,na.rm=T))) %>%
+        gather(3:ncol(.),key="dataset",value="damage") %>%
+        ungroup()
+    return(output)
+}
+
 aed_bias <- function(df) {
     # put each model in a column
     df_temp <- df %>% spread(dataset, damage)
